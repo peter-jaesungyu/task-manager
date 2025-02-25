81e9fce95db07a04ab389f4c687c0778602fbba9
jaesung <peter.yu.0402@gmail.com>
2025-02-25 17:24:41 +0900

    feat: Adding a script to run this program

---
+++ task-cli.bat
@@ -0,0 +1,3 @@
+@echo off
+title This is task-cli application
+java -classpath C:\dev\toy_projects\task-cli\out\production\task-cli TaskCliApplication %1 %2 %3
---
+++ task-cli.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+# Execute Java program
+java -classpath "C:\dev\toy_projects\task-cli\out\production\task-cli" TaskCliApplication $1 $2 $3
